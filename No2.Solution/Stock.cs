04b5a03e039f3fe670d88cc7043b34790a7a831b
Sergei Petrashko <sergei.petrashko8@gmail.com>
2018-11-26 09:32:26 +0300

    Task 3: Added solution No2

---
+++ No2.Solution/Stock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace No2.Solution
+{
+    public class Stock
+    {
+        private StockInfo _stocksInfo;
+
+        public event EventHandler<StockInfo> Changed = delegate { };
+
+        public void Market()
+        {
+            var rnd = new Random();
+
+            _stocksInfo = new StockInfo
+            {
+                Euro = rnd.Next(30, 50),
+                USD = rnd.Next(20, 40)
+            };
+
+            OnChanged(_stocksInfo);
+        }
+
+        protected virtual void OnChanged(StockInfo e)
+        {
+            Changed(this, e);
+        }
+    }
+}
