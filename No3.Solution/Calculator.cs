54a6102c9c6718e84cb4cc3b6266aaeaae70e1b9
VitaPetrovskaya <vita.petrovskaya@gmail.com>
2018-11-26 11:44:43 +0300

    Third task first option

---
+++ No3.Solution/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No3.Solution
+{
+     public class Calculator
+     {
+          public double CalculateAverage(List<double> values, IAveragingMethod averagingMethod)
+          {
+               if (values == null)
+               {
+                    throw new ArgumentNullException(nameof(values));
+               }
+
+               if (averagingMethod == null)
+               {
+                    throw new ArgumentNullException();
+               }
+
+               return averagingMethod.Count(values);
+          }
+     }
+}
