66b708d88089ca4fd95a671fdb1cf3a078ce602d
LoOgin <agrail111@yandex.ru>
2018-11-26 09:52:47 +0300

    No1.Solution

---
+++ No1.Solution.Tests/PasswordCheckerService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using No1.Solution;
+using NUnit.Framework;
+
+namespace No1.Solution.Tests
+{
+    [TestFixture]
+    public class PasswordCheckerService
+    {
+        [Test]
+        public void CheckResult()
+        {
+            No1.Solution.IChecker checker = new No1.Solution.Checker();
+            No1.Solution.ISqlRepository sqlRepository = new No1.Solution.SqlRepository();
+            No1.Solution.PasswordCheckerService passwordCheckerService = new No1.Solution.PasswordCheckerService(sqlRepository, checker);
+
+            passwordCheckerService.CheckPassword("dfsaGJifhiefh1231244");
+
+            bool resilt = passwordCheckerService.ToRepository(0);
+
+            Assert.AreEqual(true, resilt);
+        }
+    }
+}
---
+++ No1.Solution/Checker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No1.Solution
+{
+    public class Checker : IChecker
+    {
+        public CheckerResult VerifyPassword(string password)
+        {
+            if (password == null)
+                throw new ArgumentException($"{password} is null arg");
+
+            if (password == string.Empty)
+                return new CheckerResult(false, $"{password} is empty", password);
+
+            // check if length more than 7 chars 
+            if (password.Length <= 7)
+                return new CheckerResult(false, $"{password} length too short", password);
+
+            // check if length more than 10 chars for admins
+            if (password.Length >= 15)
+                return new CheckerResult(false, $"{password} length too long",password);
+
+            // check if password contains at least one alphabetical character 
+            if (!password.Any(char.IsLetter))
+                return new CheckerResult(false, $"{password} hasn't alphanumerical chars",password);
+
+            // check if password contains at least one digit character 
+            if (!password.Any(char.IsNumber))
+                return new CheckerResult(false, $"{password} hasn't digits", password);
+
+            //repository.Create(password);
+
+            return new CheckerResult(true, "Password is Ok. User was created", password);
+        }
+    }
+}
