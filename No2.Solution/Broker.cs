520bf3f0f5ffabdd08ab12740f4d49c3cac24e7a
Vitale Petrovich <43952517+VitalePetrovich@users.noreply.github.com>
2018-11-26 10:02:36 +0300

    Add files via upload

---
+++ No2.Solution/Broker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace No2.Solution
+{
+    public class Broker
+    {
+        public string Name { get; set; }
+
+        public Broker(string name, Stock stock)
+        {
+            this.Name = name;
+            this.Register(stock);
+        }
+
+        public void Register(Stock stock) => stock.Update += HandlerUpdate;
+
+        public void Unregister(Stock stock) => stock.Update -= HandlerUpdate;
+        
+        public void HandlerUpdate(object sender, StockInfo info)
+        {
+            Console.WriteLine(
+                info.USD > 30
+                    ? $"Broker {this.Name} sells dollars; Dollar rate: {info.USD}"
+                    : $"Broker {this.Name} buys dollars; Dollar rate: {info.USD}");
+        }
+    }
+}
---
+++ No2.Solution/Stock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace No2.Solution
+{
+    public class Stock
+    {
+        private StockInfo stocksInfo;
+
+        public event EventHandler<StockInfo> Update = delegate { };
+
+        private void OnUpdate(StockInfo info)
+        {
+            Update(this, info);
+        }
+
+        public Stock()
+        {
+            stocksInfo = new StockInfo();
+        }
+       
+        public void Market()
+        {
+            Random rnd = new Random();
+            stocksInfo.USD = rnd.Next(20, 40);
+            stocksInfo.Euro = rnd.Next(30, 50);
+            OnUpdate(stocksInfo);
+        }
+    }
+}
