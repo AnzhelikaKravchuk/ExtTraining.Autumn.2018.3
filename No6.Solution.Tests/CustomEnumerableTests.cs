3e88f8224e100d6109e745f5f23f00054337df71
YauheniZubko <zhenya.zubko.1999@gmail.com>
2018-11-26 11:08:05 +0300

    task 6

--- No6.Solution.Tests/CustomEnumerableTests.cs
+++ No6.Solution.Tests/CustomEnumerableTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 
 namespace No6.Solution.Tests
 {
@@ -8,9 +9,11 @@ namespace No6.Solution.Tests
         [Test]
         public void Generator_ForSequence1()
         {
-            int[] expected = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+            int[] expected = { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
 
-            Assert.Inconclusive();
+            var result = Generator.Generate(expected.Length, (a, b) => a + b, expected[0], expected[1]);
+
+            CollectionAssert.AreEqual(expected, result.ToArray());
         }
 
         [Test]
@@ -18,7 +21,9 @@ namespace No6.Solution.Tests
         {
             int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
 
-            Assert.Inconclusive();
+            var result = Generator.Generate(expected.Length, (a, b) => 6 * b - 8 * a, expected[0], expected[1]);
+
+            CollectionAssert.AreEqual(expected, result.ToArray());
         }
 
         [Test]
@@ -26,7 +31,12 @@ namespace No6.Solution.Tests
         {
             double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
 
-            Assert.Inconclusive();
+            var result = Generator.Generate(expected.Length, (a, b) => b + a / b, expected[0], expected[1]).ToArray();
+
+            for (int i = 0; i < result.ToArray().Length; i++)
+            {
+                Assert.AreEqual(expected[i], result[i], 0.0001);
+            }
         }
     }
 }
---
+++ No6/Generator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No6
+{
+    public static class Generator
+    {
+        public static IEnumerable<T> Generate<T>(int count, Func<T, T, T> function, T first, T second)
+        {
+            if (count <= 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(count)} must be positive");
+            }
+
+            IEnumerable<T> Calculate()
+            {
+                T current = first;
+                T next = second;
+
+                while (count-- > 0)
+                {
+                    yield return current;
+                    var last = current;
+                    current = next;
+                    next = function(last, current);                   
+                }
+            }
+
+            return Calculate();
+        }
+    }
+}
