5e7d50941eeb0b4ed78976db0d1bbfa4e98d701f
VitaPetrovskaya <vita.petrovskaya@gmail.com>
2018-11-26 11:25:31 +0300

    Second task

---
+++ No2.Solution/Observable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No2.Solution
+{
+     /// <summary>
+     /// Base class for Bank and Broker. Class contains method, which calls methods subscribed on event NewMarket.
+     /// </summary>
+     public class Observable
+     {
+          public virtual void Register(IObserver observer) { }
+          public virtual void Unregister(IObserver observer) { }
+          public delegate void NewMarketEventHandler(object stocksInfo);
+          public event NewMarketEventHandler NewMarket;
+
+          protected void OnNewMarket(object stocksInfo)
+          {
+               NewMarket?.Invoke(stocksInfo);
+          }
+     }
+}
