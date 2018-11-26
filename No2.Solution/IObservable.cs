59ebfeb1d0ee5b4ab486bc7621071b58e803d1b6
PrivalovIlyxa <holyhead234@mail.ru>
2018-11-26 11:23:30 +0300

    No2 task complete.

---
+++ No2.Solution/IObservable.cs
@@ -0,0 +1,9 @@
+﻿namespace No2.Solution
+{
+    public interface IObservable
+    {
+        void Register(Stock.StockChangeEventHadler handler);
+        void Unregister(Stock.StockChangeEventHadler handler);
+        void OnMarketChange(object sener, StockInfo e);
+    }
+}
