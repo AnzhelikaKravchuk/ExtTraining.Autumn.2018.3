3fa0690c4580ace01c42da432914603f3e2f4ea9
Unknown <kaledaviktoriya@gmail.com>
2018-11-26 11:07:42 +0300

    Complete the fourth task

---
+++ No4.Solution/FileGeneratorBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace No4.Solution
+{
+	public abstract class FileGeneratorBase
+	{
+		public abstract string WorkingDirectory { get; }
+
+		public abstract string FileExtension { get; }
+
+		public virtual void GenerateFiles(int filesCount, int contentLength)
+		{
+			for (var i = 0; i < filesCount; ++i)
+			{
+				var generatedFileContent = this.GenerateFileContent(contentLength);
+
+				var generatedFileName = $"{Guid.NewGuid()}{this.FileExtension}";
+
+				this.WriteBytesToFile(generatedFileName, generatedFileContent);
+			}
+		}
+
+		protected virtual void WriteBytesToFile(string fileName, byte[] content)
+		{
+			if (!Directory.Exists(WorkingDirectory))
+			{
+				Directory.CreateDirectory(WorkingDirectory);
+			}
+
+			File.WriteAllBytes($"{WorkingDirectory}//{fileName}", content);
+		}
+
+		protected abstract byte[] GenerateFileContent(int contentLength);
+	}
+}
