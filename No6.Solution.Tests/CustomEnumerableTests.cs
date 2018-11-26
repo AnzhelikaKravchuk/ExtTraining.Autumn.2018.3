785f7684f6dbc0c18b8b12466b16a3bd983436df
Unknown <kaledaviktoriya@gmail.com>
2018-11-26 11:36:03 +0300

    Complete the 6 task

--- No6.Solution.Tests/CustomEnumerableTests.cs
+++ No6.Solution.Tests/CustomEnumerableTests.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace No6.Solution.Tests
 {
@@ -10,23 +12,29 @@ namespace No6.Solution.Tests
         {
             int[] expected = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
 
-            Assert.Inconclusive();
+			var generator = new SequenceGenerator<int>(1, 1, (a, b) => a + b);
+
+			CollectionAssert.AreEqual(generator.Generate(10), expected);
         }
 
-        [Test]
-        public void Generator_ForSequence2()
-        {
-            int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
+		[Test]
+		public void Generator_ForSequence2()
+		{
+			int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
 
-            Assert.Inconclusive();
-        }
+			var generator = new SequenceGenerator<int>(1, 2, (a, b) => 6 * a - 8 * b);
 
-        [Test]
-        public void Generator_ForSequence3()
-        {
-            double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
+			CollectionAssert.AreEqual(generator.Generate(10), expected);
+		}
 
-            Assert.Inconclusive();
-        }
-    }
+		[Test]
+		public void Generator_ForSequence3()
+		{
+			double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
+
+			var generator = new SequenceGenerator<double>(1, 2, (a, b) => a + b / a);
+
+			CollectionAssert.AreEqual(generator.Generate(10), expected, Comparer<double>.Create((a, b) => Math.Abs(a - b) <= 0.000000001 ? 0 : 1));
+		}
+	}
 }
---
+++ No6/SequenceGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace No6
+{
+	public class SequenceGenerator<T>
+	{
+		public T FirstItem { get; }
+
+		public T SecondItem { get; }
+
+		public Func<T, T, T> Func { get; }
+
+		public SequenceGenerator(T firstItem, T secondItem, Func<T, T, T> func)
+		{
+			FirstItem = firstItem;
+			SecondItem = secondItem;
+			Func = func;
+		}
+
+		public IEnumerable<T> Generate(int number)
+		{
+			if (number < 2)
+				throw new ArgumentException("Positive integer number expected.", nameof(number));
+
+			T a = FirstItem;
+			T b = SecondItem;
+
+			while (number-- > 0)
+			{
+				yield return a;
+
+				T temp = a;
+				a = b;
+				b = Func(b, temp);
+			}
+		}
+	}
+}
