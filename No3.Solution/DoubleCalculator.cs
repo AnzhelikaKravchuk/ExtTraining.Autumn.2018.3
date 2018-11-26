b1a4b47a3b59171cf0fbb113833e8f9c9b59f6e9
AliceInHell <35768088+AliceInHell@users.noreply.github.com>
2018-11-26 11:11:32 +0300

    Task No3 done

---
+++ No3.Solution/DoubleCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No3.Solution
+{
+    public class DoubleCalculator
+    {
+        public double CalculateAverage(List<double> values, ICalculator calculatingLogic)
+        {
+            if (values == null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            return calculatingLogic.Calculate(values);
+        }
+
+        public double CalculateAverage(List<double> values, Func<List<double>, double> calculatingLogic)
+        {
+            if (values == null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            return calculatingLogic(values);
+        }
+    }
+}
