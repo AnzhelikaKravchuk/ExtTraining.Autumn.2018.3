973211a6f7bd5aa5af6a9c877c763c29e46a30f3
Kirill <kfidziukevich@gmail.com>
2018-11-26 10:37:50 +0300

    Task 3 done

--- No3.Solution.Tests/TestCalculator.cs
+++ No3.Solution.Tests/TestCalculator.cs
@@ -9,25 +9,53 @@ namespace No3.Solution.Tests
         private readonly List<double> values = new List<double> { 10, 5, 7, 15, 13, 12, 8, 7, 4, 2, 9 };
 
         [Test]
-        public void Test_AverageByMean()
+        public void Test_AverageByMeanInterface()
         {
             Calculator calculator = new Calculator();
+            IAveragingMethod method = new MeanMethod();
 
             double expected = 8.3636363;
 
-            double actual = calculator.CalculateAverage(values, AveragingMethod.Mean);
+            double actual = calculator.CalculateAverage(values, method);
 
             Assert.AreEqual(expected, actual, 0.000001);
         }
 
         [Test]
-        public void Test_AverageByMedian()
+        public void Test_AverageByMeanDelegate()
         {
             Calculator calculator = new Calculator();
+            IAveragingMethod method = new MeanMethod();
+
+            double expected = 8.3636363;
+
+            double actual = calculator.CalculateAverage(values, method.Calculate);
+
+            Assert.AreEqual(expected, actual, 0.000001);
+        }
+
+        [Test]
+        public void Test_AverageByMedianInterface()
+        {
+            Calculator calculator = new Calculator();
+            IAveragingMethod method = new MedianMethod();
 
             double expected = 8.0;
 
-            double actual = calculator.CalculateAverage(values, AveragingMethod.Median);
+            double actual = calculator.CalculateAverage(values, method);
+
+            Assert.AreEqual(expected, actual, 0.000001);
+        }
+
+        [Test]
+        public void Test_AverageByMedianDelegate()
+        {
+            Calculator calculator = new Calculator();
+            IAveragingMethod method = new MedianMethod();
+
+            double expected = 8.0;
+
+            double actual = calculator.CalculateAverage(values, method.Calculate);
 
             Assert.AreEqual(expected, actual, 0.000001);
         }
---
+++ No3.Solution/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace No3.Solution
+{
+    public class Calculator
+    {
+        public double CalculateAverage(List<double> values, IAveragingMethod averagingMethod)
+        {
+            if (values == null)
+            {
+                throw  new ArgumentNullException(nameof(values));
+            }
+
+            if (averagingMethod == null)
+            {
+                throw  new ArgumentNullException(nameof(averagingMethod));
+            }
+
+            return averagingMethod.Calculate(values);
+        }
+
+        public double CalculateAverage(List<double> values, Func<List<double>,double> method)
+        {
+            if (values == null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            if (method == null)
+            {
+                throw new ArgumentNullException(nameof(method));
+            }
+
+            return method(values);
+        }
+    }
+}
