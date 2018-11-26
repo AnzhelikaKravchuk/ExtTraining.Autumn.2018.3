59de2c7e39965e8f9aa4daf4fdcc1ea6c9219a4d
HannaZhuravskaya <hanna.zhuravskaya@gmail.com>
2018-11-26 11:08:15 +0300

    Added task4.

--- No4.Solution.Console/Program.cs
+++ No4.Solution.Console/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace No4.Solution.Console
+﻿using No4.Solution.RandomFileGeneratorImplementations;
+
+namespace No4.Solution.Console
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var bytes = new RandomBytesFileGenerator();
+            bytes.GenerateFiles(2, 10);
+            var chars = new RandomCharsFileGenerator();
+            chars.GenerateFiles(2, 10);
+
         }
     }
 }
