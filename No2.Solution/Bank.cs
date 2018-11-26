3491ae2abeb7fbbb2ec998f7b3df06db48c59cbe
Mikita <memesnotwar@gmail.com>
2018-11-26 10:46:34 +0300

    task 2 done

---
+++ No2.Solution/Bank.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No2.Solution
+{
+    public class Bank : IObserver
+    {
+        private IObservable stock;
+
+        public string Name { get; set; }
+
+        public Bank(string name, IObservable observable)
+        {
+            this.Name = name;
+            stock = observable;
+            stock.Register(this);
+        }
+
+        public void Update(object info)
+        {
+            var stockInfo = (StockInfo)info;
+
+            Console.WriteLine(
+                stockInfo.Euro > 40
+                    ? $"Bank {this.Name} sells euros; Euro rate:{stockInfo.Euro}"
+                    : $"Bank {this.Name} is buying euros; Euro rate: {stockInfo.Euro}");
+        }
+    }
+}
---
+++ No2.Solution/Stock.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No2.Solution
+{
+    public class Stock : IObservable
+    {
+        private delegate void StocksHandler(StockInfo info);
+
+        private event StocksHandler Notifying;
+
+        private StockInfo stocksInfo;
+
+        public Stock()
+        {
+            stocksInfo = new StockInfo();
+        }
+
+        /// <summary>
+        /// Add observer to delegate list for notify
+        /// </summary>
+        /// <param name="observer"></param>
+        public void Register(IObserver observer) => Notifying += observer.Update;
+
+        /// <summary>
+        /// Remove observer form delegate list for notify
+        /// </summary>
+        public void Unregister(IObserver observer) => Notifying -= observer.Update;
+
+        public void Notify()
+        {
+            Notifying(stocksInfo);
+        }
+
+        public void Market()
+        {
+            Random rnd = new Random();
+            stocksInfo.USD = rnd.Next(20, 40);
+            stocksInfo.Euro = rnd.Next(30, 50);
+            Notify();
+        }
+    }
+}
