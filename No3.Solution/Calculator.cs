5eab51f27ece01ad2ebc441770ed9b2ada6f15a6
Sergei Petrashko <sergei.petrashko8@gmail.com>
2018-11-26 10:46:49 +0300

    Task 3: Added solution No3

---
+++ No3.Solution/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace No3.Solution
+{
+    public static class Calculator
+    {
+        public static double CalculateAverage(IEnumerable<double> values, IAveragingCalculator<double> calculator)
+        {
+            if (values == null) throw  new ArgumentNullException(nameof(values));
+            if (calculator == null) throw new ArgumentNullException(nameof(calculator));
+
+            return CalculateAverage(values, calculator.Calculate);
+        }
+
+        public static double CalculateAverage(IEnumerable<double> values, Func<IEnumerable<double>, double> calculator)
+        {
+            if (values == null) throw new ArgumentNullException(nameof(values));
+            if (calculator == null) throw new ArgumentNullException(nameof(calculator));
+
+            return calculator(values);
+        }
+    }
+}
---
+++ No3.Solution/IAveragingCalculator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace No3.Solution
+{
+    public interface IAveragingCalculator<T>
+    {
+        T Calculate(IEnumerable<T> numbers);
+    }
+}
