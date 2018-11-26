605f60bc7f68a206c6320a7b5d3a309259f4c112
Stanislav Koburneev <koburneev.stanislav@gmail.com>
2018-11-26 11:40:36 +0300

    Added task 5.

--- No5.Solution.Console/Program.cs
+++ No5.Solution.Console/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace No5.Solution.Console
+﻿using System.Collections.Generic;
+
+namespace No5.Solution.Console
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<DocumentPart> docParts = new List<DocumentPart>();
+
+            Hyperlink link = new Hyperlink();
+            link.Url = "epam.by";
+
+            BoldText bold = new BoldText();
+            bold.Text = "Bold";
+
+            PlainText plainText = new PlainText();
+            plainText.Text = "adsadas";
+
+            docParts.Add(link);
+            docParts.Add(bold);
+            docParts.Add(plainText);
+
+            Document document = new Document(docParts);
+
+            string result = document.GetDocumentPart(p => p.ToPlainText());
         }
     }
 }
---
+++ No5.Solution/Document.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace No5.Solution
+{
+    public class Document
+    {
+        private readonly List<DocumentPart> parts;
+
+        public Document(IEnumerable<DocumentPart> parts)
+        {
+            if (parts == null)
+            {
+                throw new ArgumentNullException(nameof(parts));
+            }
+            this.parts = new List<DocumentPart>(parts);
+        }
+
+        public string GetDocumentPart(Func<DocumentPart, string> func)
+        {
+            string output = string.Empty;
+
+            foreach (DocumentPart part in parts)
+            {
+                output += func(part);
+            }
+
+            return output;
+        }
+    }
+}
