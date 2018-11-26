fbcceda6d5fc65345273686ce4b9be7398a22823
Kirill <kfidziukevich@gmail.com>
2018-11-26 10:03:18 +0300

    Task 2 done

---
+++ No2.Solution/Broker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace No2.Solution
+{
+    public class Broker : IObserver
+    {
+        private IObservable stock;
+
+        public string Name { get; set; }
+
+        public Broker(string name, IObservable observable)
+        {
+            this.Name = name;
+            stock = observable;
+            stock.Market += Update;
+        }
+
+        public void Update(object sender, MarketEventArgs e)
+        {
+            Console.WriteLine(
+                e.USD > 30
+                    ? $"Broker {this.Name} sells dollars; Dollar rate: {e.USD}"
+                    : $"Broker {this.Name} buys dollars; Dollar rate: {e.USD}");
+        }
+
+        public void StopTrade()
+        {
+            stock.Market -= Update;
+            stock = null;
+        }
+    }
+}
---
+++ No2.Solution/Stock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace No2.Solution
+{
+    public class Stock : IObservable
+    {
+        public event EventHandler<MarketEventArgs> Market;
+
+        public void Work()
+        {
+            Random rnd = new Random();
+            OnMarket(new MarketEventArgs(rnd.Next(20, 40), rnd.Next(30, 50)));
+        }
+
+        private void OnMarket(MarketEventArgs args)
+        {
+            Market?.Invoke(this, args);
+        }
+    }
+}
