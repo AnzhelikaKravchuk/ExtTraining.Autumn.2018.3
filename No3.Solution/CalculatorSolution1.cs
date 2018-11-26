863f8c53a464a0796033f0fbaa0040378245cae0
PrivalovIlyxa <holyhead234@mail.ru>
2018-11-26 11:58:45 +0300

    No3 task done.

---
+++ No3.Solution/CalculatorSolution1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace No3.Solution
+{
+    public class CalculatorSolution1
+    {
+        public double CalculateAverage(List<double> values, IAverageCaculator calculator)
+        {
+            if (values == null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            if (calculator == null)
+            {
+                throw new ArgumentNullException(nameof(calculator));
+            }
+
+            return calculator.Calculate(values);         
+        }
+    }
+}
