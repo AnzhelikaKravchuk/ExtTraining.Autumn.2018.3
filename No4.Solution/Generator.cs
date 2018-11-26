a988ee6063fd9c873e31390c568774abfeb4fc88
LoOgin <agrail111@yandex.ru>
2018-11-26 11:55:27 +0300

    No4.Solution

---
+++ No4.Solution/Generator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No4.Solution
+{
+    public class Generator: AbstractGenerator
+    {
+        private IRandom random;
+
+        public Generator(IRandom random)
+        {
+            this.random = random ?? throw new ArgumentNullException(nameof(random));
+        }
+    }
+}
---
+++ No4.Solution/Randomizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No4.Solution
+{
+    public class Randomizer : IRandom
+    {
+        public byte[] Random(int size)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
