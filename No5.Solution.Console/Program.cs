4d864c89b33a2909136546c104aceddfa5051826
Kirill <kfidziukevich@gmail.com>
2018-11-26 11:56:17 +0300

    Task 5 done

--- No5.Solution.Console/Program.cs
+++ No5.Solution.Console/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace No5.Solution.Console
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No5.Solution.Console
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Document document = new Document(new List<DocumentPart> { new PlainText() { Text = "efwfwefwe" },
+                                             new Hyperlink() { Text = "krwjg" },
+                                             new PlainText() { Text = "ewerefwe" },
+                                             new BoldText() { Text = "eml;m;e" } } );
+
+            System.Console.WriteLine(document.Convert(new ToHtmlConverter()));
+            System.Console.Read();
         }
     }
 }
---
+++ No5.Solution/Document.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace No5.Solution
+{
+    public class Document
+    {
+        private readonly List<DocumentPart> parts;
+
+        public Document(IEnumerable<DocumentPart> parts)
+        {
+            if (parts == null)
+            {
+                throw new ArgumentNullException(nameof(parts));
+            }
+            this.parts = new List<DocumentPart>(parts);
+        }
+
+        public string Convert(IConverter converter)
+        {
+            if (converter == null)
+                throw new ArgumentNullException();
+
+            return converter.Convert(parts);
+        }
+    }
+}
