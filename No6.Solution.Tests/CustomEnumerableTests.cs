09ebae7fb9b93e032670c314b141637ffafc4712
yuya-yuliya <yuya.yuliya@gmail.com>
2018-11-26 09:30:59 +0300

    Complete task No.6

--- No6.Solution.Tests/CustomEnumerableTests.cs
+++ No6.Solution.Tests/CustomEnumerableTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 
 namespace No6.Solution.Tests
 {
@@ -9,24 +10,42 @@ namespace No6.Solution.Tests
         public void Generator_ForSequence1()
         {
             int[] expected = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+            Generator<int> generator = new Generator<int>((a, b) => a + b);
+            int first = 1;
+            int second = 1;
+            int count = 10;
 
-            Assert.Inconclusive();
+            int[] actual = generator.Generate(count, first, second).ToArray();
+
+            Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void Generator_ForSequence2()
         {
             int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
+            Generator<int> generator = new Generator<int>((a, b) => 6 * a - 8 * b);
+            int first = 1;
+            int second = 2;
+            int count = 10;
 
-            Assert.Inconclusive();
+            int[] actual = generator.Generate(count, first, second).ToArray();
+
+            Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void Generator_ForSequence3()
         {
             double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
+            Generator<double> generator = new Generator<double>((a, b) => a + b / a);
+            double first = 1;
+            double second = 2;
+            int count = 10;
 
-            Assert.Inconclusive();
+            double[] actual = generator.Generate(count, first, second).ToArray();
+
+            Assert.That(actual, Is.EqualTo(expected).Within(0.01));
         }
     }
 }
---
+++ No6/Generator(T).cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No6
+{
+    public class Generator<T>
+    {
+        private Func<T, T, T> generatorFunc;
+
+        public Generator(Func<T, T, T> generatorFunc)
+        {
+            this.generatorFunc = generatorFunc;
+        }
+
+        public IEnumerable<T> Generate(int count, T first, T second)
+        {
+            T curr = second;
+            T prev = first;
+            for (int i = 0; i < count; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        yield return prev;
+                        break;
+                    case 1:
+                        yield return curr;
+                        break;
+                    default:
+                        {
+                            T temp = generatorFunc(curr, prev);
+                            prev = curr;
+                            curr = temp;
+                            yield return curr;
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
