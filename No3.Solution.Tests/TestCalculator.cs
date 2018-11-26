489b96cf77a1a593dc80e32aa49673f1359aaba2
LoOgin <agrail111@yandex.ru>
2018-11-26 11:38:08 +0300

    No3.Solution

--- No3.Solution.Tests/TestCalculator.cs
+++ No3.Solution.Tests/TestCalculator.cs
@@ -6,16 +6,16 @@ namespace No3.Solution.Tests
     [TestFixture]
     public class TestCalculator
     {
-        private readonly List<double> values = new List<double> { 10, 5, 7, 15, 13, 12, 8, 7, 4, 2, 9 };
+        private readonly List<double> values = new List<double> { 10, 5, 7, 15, 13, 12, 8, 7, 4, 2, 9 };        
 
         [Test]
         public void Test_AverageByMean()
         {
-            Calculator calculator = new Calculator();
+            Calculator calculator = new Calculator(new AveragingMethodMean());
 
             double expected = 8.3636363;
 
-            double actual = calculator.CalculateAverage(values, AveragingMethod.Mean);
+            double actual = calculator.CalculateAverage(values);
 
             Assert.AreEqual(expected, actual, 0.000001);
         }
@@ -23,11 +23,11 @@ namespace No3.Solution.Tests
         [Test]
         public void Test_AverageByMedian()
         {
-            Calculator calculator = new Calculator();
+            Calculator calculator = new Calculator(new AveragingMethodMediancs());
 
             double expected = 8.0;
 
-            double actual = calculator.CalculateAverage(values, AveragingMethod.Median);
+            double actual = calculator.CalculateAverage(values);
 
             Assert.AreEqual(expected, actual, 0.000001);
         }
---
+++ No3.Solution/AveragingMethodMean.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No3.Solution
+{
+    public class AveragingMethodMean : ICalculate
+    {
+        public double Calculate(List<double> values)
+        {
+            return values.Sum() / values.Count;
+        }
+       
+    }
+}
