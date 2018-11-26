4c57bc1b9103f590e326bf75bf00609caea9a232
yuya-yuliya <yuya.yuliya@gmail.com>
2018-11-26 09:53:29 +0300

    Complete task No.4

--- No4.Solution.Console/Program.cs
+++ No4.Solution.Console/Program.cs
@@ -4,6 +4,19 @@
     {
         static void Main(string[] args)
         {
+            IFileGenerator[] generators = { new RandomBytesFileGenerator(), new RandomCharsFileGenerator() };
+            foreach (var generator in generators)
+            {
+                GenerateFiles(generator, 10, 10);
+            }
+
+            System.Console.ReadLine();
+        }
+
+        static void GenerateFiles(IFileGenerator generator, int count, int length)
+        {
+            generator.GenerateFiles(count, length);
+            System.Console.WriteLine($"Was generated {count} files, {length} length each in directory \"{generator.WorkingDirectory}\". Files have extension \"{generator.FileExtension}\"");
         }
     }
 }
---
+++ No4.Solution/IFileGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace No4.Solution
+{
+    public interface IFileGenerator
+    {
+        string WorkingDirectory { get; }
+        string FileExtension { get; }
+        void GenerateFiles(int filesCount, int contentLength);
+    }
+}
